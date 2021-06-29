03cd1f8b9a97e30e29b468e02ca95bc7275d0ace
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-06-29 14:46:39 +0100

    Patch out ICAT connections

--- autoreduce_rest_api/runs/test/test_submit_runs.py
+++ autoreduce_rest_api/runs/test/test_submit_runs.py
@@ -1,12 +1,17 @@
 from autoreduce_utils.clients.connection_exception import ConnectionException
 import requests
+import os
 import time
 from django.contrib.auth import get_user_model
 from django.test import LiveServerTestCase
 from rest_framework.authtoken.models import Token
+from unittest.mock import Mock, patch
 
 from autoreduce_db.reduction_viewer.models import ReductionRun
 from autoreduce_qp.queue_processor.queue_listener import setup_connection
+from autoreduce_utils.settings import SCRIPTS_DIRECTORY
+
+INSTRUMENT_NAME = "TESTINSTRUMENT"
 
 
 def wait_until(somepredicate, timeout=30, period=0.25, *args, **kwargs):
@@ -31,6 +36,11 @@ class SubmitRunsTest(LiveServerTestCase):
         except ConnectionException as err:
             raise RuntimeError("Could not connect to ActiveMQ - check your credentials. If running locally check that "
                                "ActiveMQ is running and started by `python setup.py start`") from err
+
+        os.makedirs(SCRIPTS_DIRECTORY % INSTRUMENT_NAME, exist_ok=True)
+        with open(os.path.join(SCRIPTS_DIRECTORY % INSTRUMENT_NAME, "reduce_vars.py"), 'w') as file:
+            file.write("")
+
         return super().setUpClass()
 
     def setUp(self) -> None:
@@ -38,15 +48,25 @@ class SubmitRunsTest(LiveServerTestCase):
         self.token = Token.objects.create(user=user.objects.first())
         return super().setUp()
 
-    def test_submit_and_delete_run_range(self):
+    @patch('autoreduce_scripts.manual_operations.manual_submission.login_icat')
+    @patch('autoreduce_scripts.manual_operations.manual_submission.get_location_and_rb_from_icat',
+           return_value=["/tmp/location", "RB1234567"])
+    def test_submit_and_delete_run_range(self, login_icat: Mock, get_location_and_rb_from_icat: Mock):
         """
         Submit and delete a run range via the API
         """
-        response = requests.post(f"{self.live_server_url}/api/runs/INTER/63125/63130",
+        response = requests.post(f"{self.live_server_url}/api/runs/{INSTRUMENT_NAME}/63125/63130",
                                  headers={"Authorization": f"Token {self.token}"})
         assert response.status_code == 200
-        assert wait_until(lambda: ReductionRun.objects.count() == 5)
-        response = requests.delete(f"{self.live_server_url}/api/runs/INTER/63125/63130",
+        assert wait_until(lambda: ReductionRun.objects.count() == 6)
+        login_icat.assert_called_once()
+        login_icat.reset_mock()
+        assert get_location_and_rb_from_icat.call_count == 5
+        get_location_and_rb_from_icat.reset_mock()
+
+        response = requests.delete(f"{self.live_server_url}/api/runs/{INSTRUMENT_NAME}/63125/63130",
                                    headers={"Authorization": f"Token {self.token}"})
         assert response.status_code == 200
         assert wait_until(lambda: ReductionRun.objects.count() == 0)
+        login_icat.assert_not_called()
+        get_location_and_rb_from_icat.assert_not_called()
