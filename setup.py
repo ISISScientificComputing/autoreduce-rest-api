fcb88ae4b3bfbd8a9afafe1d987e099271f75e82
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-12 14:52:40 +0100

    Use newer scripts version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ for more details
 from setuptools import setup, find_packages
 
 setup(name="autoreduce_rest_api",
-      version="22.0.0.dev8",
+      version="22.0.0.dev9",
       description="ISIS Autoreduction Runs REST API",
       author="ISIS Autoreduction Team",
       url="https://github.com/ISISScientificComputing/autoreduce/",
-      install_requires=["autoreduce_scripts==22.0.0.dev18", "django==3.2.8", "djangorestframework==3.12.4"],
+      install_requires=["autoreduce_scripts==22.0.0.dev20", "django==3.2.8", "djangorestframework==3.12.4"],
       packages=find_packages(),
       entry_points={"console_scripts": ["autoreduce-rest-api-manage = autoreduce_rest_api.manage:main"]},
       classifiers=[
