14d2a3d6073c8567a72a3ce010d7dd636bc8267d
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-18 09:51:50 +0100

    Change tests to use a mysql db backend

--- autoreduce_rest_api/runs/test/test_submit_runs.py
+++ autoreduce_rest_api/runs/test/test_submit_runs.py
@@ -40,6 +40,12 @@ class SubmitRunsTest(LiveServerTestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        try:
+            cls.queue_client, cls.listener = setup_connection()
+        except ConnectionException as err:
+            raise RuntimeError("Could not connect to ActiveMQ - check your credentials. If running locally check that "
+                               "ActiveMQ Docker container is running and started") from err
+
         os.makedirs(SCRIPTS_DIRECTORY % INSTRUMENT_NAME, exist_ok=True)
         with open(os.path.join(SCRIPTS_DIRECTORY % INSTRUMENT_NAME, "reduce_vars.py"), 'w') as file:
             file.write("")
@@ -47,19 +53,23 @@ class SubmitRunsTest(LiveServerTestCase):
         return super().setUpClass()
 
     def setUp(self) -> None:
-        try:
-            self.queue_client, self.listener = setup_connection()
-        except ConnectionException as err:
-            raise RuntimeError("Could not connect to ActiveMQ - check your credentials. If running locally check that "
-                               "ActiveMQ Docker container is running and started") from err
         user = get_user_model()
         self.token = Token.objects.create(user=user.objects.first())
         return super().setUp()
 
+    @patch('autoreduce_scripts.manual_operations.manual_submission.get_run_data_from_database',
+           return_value=[None, None, None])
     @patch('autoreduce_scripts.manual_operations.manual_submission.get_run_data_from_icat',
            return_value=["/tmp/location", "RB1234567", "test_title"])
-    def test_submit_and_delete_run_range(self, get_run_data_from_icat: Mock):
-        """Submit and delete a run range via the API."""
+    def test_submit_and_delete_run_range(self, get_run_data_from_database: Mock, get_run_data_from_icat: Mock):
+        """Submit and delete a run range via the API.
+
+        Args:
+            get_run_data_from_database: Mocks the function to call the db, as this can overload
+                                        SQLite3 in testing with too much parallelism
+                                        (queue processor writing, api reading), which just throws up an error
+            get_run_data_from_icat: Mocks the function to call ICAT, to avoid unnecessary calls to their service
+        """
         response = requests.post(f"{self.live_server_url}/api/runs/{INSTRUMENT_NAME}",
                                  json={
                                      "runs": list(range(63125, 63131)),
@@ -67,7 +77,9 @@ class SubmitRunsTest(LiveServerTestCase):
                                  headers={"Authorization": f"Token {self.token}"})
         assert response.status_code == 200
         assert wait_until(lambda: ReductionRun.objects.count() == 6)
+        assert get_run_data_from_database.call_count == 6
         assert get_run_data_from_icat.call_count == 6
+        get_run_data_from_database.reset_mock()
         get_run_data_from_icat.reset_mock()
 
         response = requests.delete(f"{self.live_server_url}/api/runs/{INSTRUMENT_NAME}",
@@ -77,4 +89,40 @@ class SubmitRunsTest(LiveServerTestCase):
                                    headers={"Authorization": f"Token {self.token}"})
         assert response.status_code == 200
         assert wait_until(lambda: ReductionRun.objects.count() == 0)
+        get_run_data_from_database.assert_not_called()
+        get_run_data_from_icat.assert_not_called()
+
+    @patch('autoreduce_scripts.manual_operations.manual_submission.get_run_data_from_database',
+           return_value=[None, None, None])
+    @patch("autoreduce_scripts.manual_operations.manual_submission.get_run_data_from_icat",
+           return_value=["/tmp/location", "RB1234567", "test_title"])
+    def test_batch_submit_and_delete_run(self, get_run_data_from_database: Mock, get_run_data_from_icat: Mock):
+        """Submit and delete a run range via the API."""
+        response = requests.post(f"{self.live_server_url}/api/runs/batch/{INSTRUMENT_NAME}",
+                                 headers={"Authorization": f"Token {self.token}"},
+                                 json={
+                                     "runs": [63125, 63130],
+                                     "reduction_arguments": {
+                                         "apple": "banana"
+                                     },
+                                     "user_id": 99199,
+                                     "description": "Test description"
+                                 })
+        assert response.status_code == 200
+        assert wait_until(lambda: ReductionRun.objects.count() == 1)
+        assert get_run_data_from_database.call_count == 2
+        assert get_run_data_from_icat.call_count == 2
+        get_run_data_from_icat.reset_mock()
+        get_run_data_from_database.reset_mock()
+
+        reduced_run = ReductionRun.objects.first()
+        assert reduced_run.started_by == 99199
+        assert reduced_run.run_description == "Test description"
+
+        response = requests.delete(f"{self.live_server_url}/api/runs/batch/{INSTRUMENT_NAME}",
+                                   json={"runs": [reduced_run.pk]},
+                                   headers={"Authorization": f"Token {self.token}"})
+        assert response.status_code == 200
+        assert wait_until(lambda: ReductionRun.objects.count() == 0)
+        get_run_data_from_database.assert_not_called()
         get_run_data_from_icat.assert_not_called()
