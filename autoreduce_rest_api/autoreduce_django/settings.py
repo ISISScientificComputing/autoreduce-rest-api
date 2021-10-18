47432450c1e5a65d9ba1f41298e5002b6095d6f2
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-10-18 11:23:55 +0100

    Reverts database check in settings

--- autoreduce_rest_api/autoreduce_django/settings.py
+++ autoreduce_rest_api/autoreduce_django/settings.py
@@ -40,7 +40,19 @@ INSTALLED_APPS = [
     'autoreduce_rest_api.runs'
 ]
 
-DATABASES = autoreduce_db_settings
+if os.environ.get("TESTING_MYSQL_DB", None) is not None:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': "autoreduce",
+            'USER': "root",
+            'PASSWORD': "password",
+            'HOST': "127.0.0.1",
+            'PORT': "3306",
+        }
+    }
+else:
+    DATABASES = autoreduce_db_settings
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
