2710086a4126c9ea9c9349ff4b83ce43e6dd34bd
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2021-12-21 14:13:07 +0000

    Update db version, unpin django and increment current version

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ for more details
 from setuptools import setup, find_packages
 
 setup(name="autoreduce_rest_api",
-      version="22.0.0.dev12",
+      version="22.0.0.dev13",
       description="ISIS Autoreduction Runs REST API",
       author="ISIS Autoreduction Team",
       url="https://github.com/autoreduction/autoreduce/",
-      install_requires=["autoreduce_scripts==22.0.0.dev28", "django==3.2.8", "djangorestframework==3.12.4"],
+      install_requires=["autoreduce_scripts==22.0.0.dev29", "django", "djangorestframework==3.12.4"],
       packages=find_packages(),
       entry_points={"console_scripts": ["autoreduce-rest-api-manage = autoreduce_rest_api.manage:main"]},
       classifiers=[
