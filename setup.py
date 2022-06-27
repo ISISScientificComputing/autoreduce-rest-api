e91af532577e725c277ed5db456b4fbe9deee9c8
James Brookes <james.brookes@stfc.ac.uk>
2022-06-27 18:51:24 +0100

    Increment versions

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ for more details
 from setuptools import setup, find_packages
 
 setup(name="autoreduce_rest_api",
-      version="22.0.0.dev21",
+      version="22.0.0.dev22",
       description="ISIS Autoreduction Runs REST API",
       author="ISIS Autoreduction Team",
       url="https://github.com/autoreduction/autoreduce/",
-      install_requires=["autoreduce_scripts==22.0.0.dev42", "Django", "djangorestframework==3.13.1"],
+      install_requires=["autoreduce_scripts==22.0.0.dev43", "Django", "djangorestframework==3.13.1"],
       packages=find_packages(),
       entry_points={"console_scripts": ["autoreduce-rest-api-manage = autoreduce_rest_api.manage:main"]},
       classifiers=[
